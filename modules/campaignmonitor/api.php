9528ba7844ab9d1bf40a23766503190cbc0479a9
Daniel Lever <daniel@btrmlk.com>
2015-10-28 17:26:37 +1100

    Added Campaign Monitor module to plugin

---
+++ modules/campaignmonitor/api.php
@@ -0,0 +1,45 @@
+<?
+	
+	/*
+		An example of using a child controller for more complex behaviour
+	*/
+	class ED_CampaignMonitor_API extends ED_API {
+		
+		private $_loaded = false;
+		public $twitter = null;
+		
+		public function subscribeToList($args, $isAjax = true) {
+			
+			require_once("createsend-api/csrest_subscribers.php");
+			
+			$listID = $this->getListID($args['list'] ? $args['list'] : "main");
+			
+			$wrapper = new CS_REST_Subscribers($listID, array(
+				"api_key" => ED()->getModuleSetting("campaignmonitor", "apiKey")
+			));
+			
+			$result = $wrapper->add(array(
+				"EmailAddress" => $args['email'],
+				"Name" => $args['name'],
+				"CustomFields" => $args['CustomFields']
+			));
+			
+			if(!$result->was_successful()) {
+				throw new Exception("An error occurred while subscribing: (Code ".$result->response->Code.") ".$result->response->Message);
+			}
+			
+			return true;
+			
+		}
+		
+		public function getListID($listName) {
+			$lists = ED()->getModuleSetting("campaignmonitor", "listIDs");
+			dump("Getting list", $listName, $list);
+			return $lists[$listName];
+		}
+		
+	}
+	
+	ED()->API->addController("campaignmonitor", new ED_CampaignMonitor_API());
+	
+?>
---
+++ modules/campaignmonitor/module.php
@@ -0,0 +1,11 @@
+<?
+	
+	ED()->defineModule("campaignmonitor", array(
+		"title" => "Campaign Monitor",
+		"apiKey" => "faec44ef4575bf1f90e39e07e93ab5c9",
+		"listIDs" => array(
+			'main' => "ENTER_LIST_ID"
+		)
+	));
+
+?>
