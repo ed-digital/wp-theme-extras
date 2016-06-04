706842ff10082c956699a625d7879a2a8d1d5d19
ED-WP <web@ed.com.au>
2016-06-04 15:18:53 +1000

    AUTOBUMP: 1.3.24

--- edplugin.php
+++ edplugin.php
@@ -3,7 +3,7 @@
 Plugin Name: ED. Plugin
 Plugin URI: http://ed.com.au
 Description: Provides a bunch of cool stuff for ED. Developers
-Version: 1.3.23
+Version: 1.3.24
 Author: Daniel Lever
 Text Domain: edplugin
 License: GPLv2 or later.
