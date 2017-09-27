bd40a0e6777da5df2ad35802034b3411e6c3954e
Daniel Lever <daniel@ed.com.au>
2017-09-27 12:38:22 +1000

    Fixed bug with grid

--- _copy_me_/assets-src/js/index.js
+++ _copy_me_/assets-src/js/index.js
@@ -1,18 +1,18 @@
 import _babel from 'babel-polyfill';
-import Site from 'sitekit';
+import SiteInstance from 'sitekit';
 import * as widgets from "glob:./widgets/*.js";
 
 // Create new site
-const site = new Site();
+const Site = new SiteInstance();
 
 // Install globals for dev purposes
-window.Site = site;
-window.jQuery = window.$ = site.$;
+window.Site = Site;
+window.jQuery = window.$ = Site.$;
 
 // Run widget functions
 for(let item of Object.values(widgets)) {
   if(typeof item == 'function') {
-    item(site, site.$);
+    item(Site, Site.$);
   } else {
     if(window.console && window.console.error) {
       console.error("One widget file did not export a function.");
