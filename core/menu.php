ab7ec450bce49e1d308bd97946125a3fd745bd33
Daniel Lever <daniel@ed.com.au>
2018-03-07 12:14:17 +1100

    Refreshed menu code

--- core/menu.php
+++ core/menu.php
@@ -1,22 +1,44 @@
 <?
-  add_filter('nav_menu_css_class', function($classes, $item) {
+  
+  add_filter('wp_nav_menu_objects', function($items) {
     
-    // Remove existing 'current-menu-item'
-    $classes = array_filter($classes, function($val) {
-      return $val !== "current-menu-item";
-    });
-    
-    // Add new
-    if (preg_match("/^\/[a-z0-9]+/", $item->url) && strpos($_SERVER['REQUEST_URI'], $item->url) === 0) {
-      $classes[] = "current-menu-item";
+    // Remove the default current-menu-item
+    foreach ($items as $item) {
+      $classes = array_filter($item->classes, function($val) {
+        return $val !== "current-menu-item";
+      });
     }
     
-    // If the item contains custom HTML, add the html-item class
-    // dump($item);
-    if (preg_match("/<\/[a-z]+>/", $item->title)) {
-      $classes[] = "html-item";
+    $queue = [0];
+    
+    while (count($queue)) {
+      // Get the next queue item
+      $parent = array_shift($queue);
+      
+      // Figure out the best match for items in this menu branch
+      $bestMatch = null;
+      $bestMatchLength = 0;
+      $url = $_SERVER['REQUEST_URI'];
+      foreach ($items as $item) {
+        if ($item->menu_item_parent == $parent) {
+          if (strpos($url, $item->url) === 0) {
+            $len = strlen($item->url);
+            if ($len > $bestMatchLength) {
+              $bestMatchLength = $len;
+              $bestMatch = $item;
+            }
+          }
+        }
+      }
+      
+      // If we have a match, mark that item as active, and add it's children to the queue
+      if ($bestMatch) {
+        $queue[] = $bestMatch->ID;
+        $bestMatch->classes[] = 'current-menu-item';
+      }
     }
     
-    return $classes;
-  }, 1, 2);
+    return $items;
+  });
+  
 ?>
