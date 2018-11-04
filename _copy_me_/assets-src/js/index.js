2096f166eefb10fcafab93b4e8803e9ddcec56d5
Daniel Lever <daniel@ed.com.au>
2018-11-05 09:32:06 +1100

    Updated sample js

--- _copy_me_/assets-src/js/index.js
+++ _copy_me_/assets-src/js/index.js
@@ -1,21 +1,21 @@
-import _babel from 'babel-polyfill';
-import SiteInstance from 'sitekit';
-import * as widgets from "glob:./widgets/*.js";
+import 'babel-polyfill'
+import SiteInstance from 'sitekit'
+import * as widgets from "glob:./widgets/*.js"
 
 // Create new site
-const Site = new SiteInstance();
+const Site = new SiteInstance()
 
 // Install globals for dev purposes
-window.Site = Site;
-window.jQuery = window.$ = Site.$;
+window.Site = Site
+window.jQuery = window.$ = Site.$
 
 // Run widget functions
 for(let item of Object.values(widgets)) {
   if(typeof item == 'function') {
-    item(Site, Site.$);
+    item(Site, Site.$)
   } else {
     if(window.console && window.console.error) {
-      console.error("One widget file did not export a function.");
+      console.error("One widget file did not export a function.")
     }
   }
 }
--- _copy_me_/assets-src/js/widgets/sample.js
+++ _copy_me_/assets-src/js/widgets/sample.js
@@ -1,74 +1,9 @@
-// import fs from 'fs';
+export default function (Site, $) {
 
-module.exports = (Site, $) => {
-  
-  /*
-  
-  Site.widget('header', {
-    _create() {
-      
-      this.element.find(".mobile-menu-toggle").click(() => {
-        $(document.body).toggleClass("nav-active");
-      });
-      
+  Site.widget('demo', {
+    _create () {
+      console.log('Hey!')
     }
-  });
-  
-  // Example markup: <div class="gallery" data-widget="gallery" data-items="[\"cool.jpg\",\"something.jpg\"]"
-  Site.widget('gallery', {
-    options: {
-      items: []     // Accepts a list of images
-    },
-    _create() {
-      
-      // Preload images :)
-      Site.preloadImages(this.options.items);
-      
-      this.currentIndex = 0;
-      this.inner = $("<div class='inner'></div>").appendTo(this.element);
-      this.frame = $("<div class='frame'></div>").appendTo(this.inner);
-      this.controls = $("<div class='controls'></div>").appendTo(this.inner);
-      
-      this.left = $("<div class='item left'></div>").appendTo(this.controls);
-      this.right = $("<div class='item right'></div>").appendTo(this.controls);
-      
-      this.left.click(() => this.go(-1));
-      this.right.click(() => this.go(1));
-      
-      this.go(0);
-      
-    },
-    go(dir) {
-      if(this.disabled) return;
-      this.disabled = true;
-      
-      this.currentIndex += dir;
-      
-      if(this.currentIndex >= this.options.length) {
-        this.currentIndex = 0;
-      } else if(this.currentIndex < 0) {
-        this.currentIndex = this.options.items.length - 1;
-      }
-      
-      this.frame.fadeOut({
-        duration: 500,
-        complete: () => {
-          
-          this.frame
-            .css('background-image', 'url("'+this.options.items[this.currentIndex].src+'")')
-            .fadeIn({
-              duration: 500,
-              complete: () => {
-                this.disabled = false;
-              }
-            });
-          
-        }
-      });
-    }
-    
-  });
-  
-  */
-  
-};
+  })
+
+}
